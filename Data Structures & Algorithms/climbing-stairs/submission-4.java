3c51e20fe74d00e376e9b961ea6b60093dddf4d6
shubhdeep123 <64921946+shubhdeep123@users.noreply.github.com>
2026-09-21 10:29:04 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    int[] dp;
+    public int climbStairs(int n) {
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        return climbStairsHelper(n);
+    }
+
+    public int climbStairsHelper(int n) {
+        if (n == 0 || n == 1) return 1;
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        dp[n] = climbStairsHelper(n-1) + climbStairsHelper(n-2);
+
+        return dp[n];
+    }
+}
